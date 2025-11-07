093410c5db430be5a2e10e04715717c9c2b9daa6
abiikoo <abigail.koo@utp.ac.pa>
2025-11-07 00:01:07 -0500

    Create seed.sql

---
+++ seed.sql
@@ -0,0 +1,8 @@
+
+USE authlab;
+
+INSERT INTO usuarios (nombre, apellido, email, sexo, password_hash)
+VALUES 
+('Usuario', 'Demo', 'demo@local.test', 'X', 
+'$2y$10$3A1FK1w2x7fYIcsqgT9IfOBsKEGcgIDz53rA1aeb2CqgU1jz8r4vG');
+
